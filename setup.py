4f38c22af152549c138196313ed9d350d2924da7
hoskim1 <michael.hoskin@ons.gov.uk>
2021-11-17 15:31:54 +0000

    included setup.py and adjusted module pathing in code
    This commit allows for the code to be installed as a package using e.g. pip install . with correct pathing for defined functions

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup for midas_pro"""
+
+import setuptools
+
+setuptools.setup(name='midas_pro',
+                 version='0.1',
+                 description='Python version of Mixed Data Sampling (MIDAS) regression (allow for multivariate MIDAS)',
+                 url='https://github.com/sapphire921/midas_pro',
+                 author='sapphire921',
+                 install_requires=[],
+                 author_email='',
+				 license='MIT',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
